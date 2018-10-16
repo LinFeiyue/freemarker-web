fb9462e9bfdd1fbbb99f5531ec92aec38fbd2550
LinFeiyue <18511755198@163.com>
2018-10-16 23:50:01 +0800

    安全验证配置

---
+++ src/com/pache/framework/web/security/filter/UsernamePasswordAuthenticationFilter.java
@@ -0,0 +1,31 @@
+package com.pache.framework.web.security.filter;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class UsernamePasswordAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+
+    protected UsernamePasswordAuthenticationFilter() {
+        super("/j_spring_security_check");
+    }
+
+    /**
+     * 该方法必须重写
+     * @param httpServletRequest
+     * @param httpServletResponse
+     * @return
+     * @throws AuthenticationException
+     * @throws IOException
+     * @throws ServletException
+     */
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws AuthenticationException, IOException, ServletException {
+        return null;
+    }
+}
